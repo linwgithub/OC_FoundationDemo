6b78a609dcb6265773c8392856675bc451cf7705
linwcoder <770601773@q.com>
2016-03-31 13:13:30 +0800

    add  project

---
+++ OC_FoundationLearnDame/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OC_FoundationLearnDame
+//
+//  Created by pollysoft on 16/3/31.
+//  Copyright © 2016年 microi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
